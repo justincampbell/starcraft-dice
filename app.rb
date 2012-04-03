439151d4ecb775f8ace244a6993f40eee23a2057
Justin Campbell <justin@justincampbell.me>
2012-04-03 09:12:28 -0400

    Generalize to dice app

--- app.rb
+++ app.rb
@@ -1,8 +1,24 @@
-get '/' do
-  @race = %w[Terran Zerg Protoss].sample
-  slim :index
-end
-
 get('/app.css') { sass   :app }
 get('/app.js')  { coffee :app }
+
+get '/:max' do
+  @result = roll 1, params[:max]
+
+  slim :dice
+end
+
+get '/:min/:max' do
+  @result = roll params[:min], params[:max]
+
+  slim :dice
+end
+
+private
+
+def roll(min, max)
+  min = min.to_i
+  max = max.to_i
+
+  rand((max - min) + 1) + min
+end
 
